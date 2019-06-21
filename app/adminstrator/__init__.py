1f504c51f8814dcc0af3066add848881dd821452
Stormryder <january_thomas@hotmail.com>
2019-06-21 15:16:52 +0300

    csrf

--- app/adminstrator/__init__.py
+++ app/adminstrator/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
+from app import csrf
 
 bp = Blueprint('administrator', __name__)
+csrf.exempt(bp)
+
 
 from app.adminstrator import routes
--- app/application/__init__.py
+++ app/application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-
+from app import  csrf
 bp = Blueprint('application', __name__)
 
+csrf.exempt(bp)
 from app.application import routes
